4c887b29dd82c96b16b4d47c66ff4f76f44aafe4
unknown <you@example.com>
2021-08-26 00:13:34 +0300

    Updates

---
+++ precache-manifest.bfb17d925067097068126a4d8cf08865.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "aec1b3b96dbcfe101837",
+    "url": "/got/static/css/main.c4b80aa3.chunk.css"
+  },
+  {
+    "revision": "aec1b3b96dbcfe101837",
+    "url": "/got/static/js/main.aec1b3b9.chunk.js"
+  },
+  {
+    "revision": "35d6d47175bd1f4ed5be",
+    "url": "/got/static/css/1.ee86bc40.chunk.css"
+  },
+  {
+    "revision": "35d6d47175bd1f4ed5be",
+    "url": "/got/static/js/1.35d6d471.chunk.js"
+  },
+  {
+    "revision": "3c9c5871ac0eaadc3a8d",
+    "url": "/got/static/js/runtime~main.3c9c5871.js"
+  },
+  {
+    "revision": "cc9a421ef13fc85f5e38f9b0964f9872",
+    "url": "/got/static/media/error.cc9a421e.jpg"
+  },
+  {
+    "revision": "4da810837c6dd45262462cccf9566c34",
+    "url": "/got/static/media/got.4da81083.jpeg"
+  },
+  {
+    "revision": "0dd40543366c4c49a94cee520ae7de03",
+    "url": "/got/index.html"
+  }
+];
